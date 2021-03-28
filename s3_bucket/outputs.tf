f761e27a14152131b6f06e0cae9358a1e1787a3d
Fernando Ruaro <landell.ruaro@gmail.com>
2021-03-28 09:54:39 -0300

    Interpolation-only expressions are deprecated

--- s3_bucket/outputs.tf
+++ s3_bucket/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket" {
-  value = "${aws_s3_bucket.bucket.bucket}"
+  value = aws_s3_bucket.bucket.bucket
 }
 
 output "id" {
-  value = "${aws_s3_bucket.bucket.id}"
+  value = aws_s3_bucket.bucket.id
 }
 
 output "arn" {
-  value = "${aws_s3_bucket.bucket.arn}"
+  value = aws_s3_bucket.bucket.arn
 }
